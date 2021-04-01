f3413e0a24e8d4e1d0835485269157a85bf4b7f4
Ghulam Nabi <khosognk@gmail.com>
2021-04-01 23:38:53 +0500

    R Ka Taruff Book

--- loop functions and control structures.R
+++ loop functions and control structures.R
@@ -46,4 +46,57 @@ x*y
 x-y
 x/y
 x %*% y
-# Data 
+# lapply loop function
+x <- list(a=1:5,b=rnorm(10))
+x
+lapply(x,sum)
+mean(x$a)
+sum(x$a)
+mean(x$b)
+# another lapply function
+
+x <- list(a=1:4,b=rnorm(10),c=rnorm(20,1),d=rnorm(100,5))
+x
+sapply(x,sum)
+mean(x$c)
+mean(x$d)
+# runif function with lapply
+x <- 1:4
+y <- sapply(x,runif,min=5,max=10)
+
+names(x) <- c("A","B","C","D")
+y
+y[[1]]
+y$A
+y$B
+y$C
+runif(3)
+y
+# sapply loop function
+
+x <- list(a=1:5,b=rnorm(10))
+x
+sapply(x,sum)
+# apply loop function
+
+x <- matrix(rnorm(200),20,10)
+x
+apply(x,1,mean)
+apply(x,2,mean)
+apply(x,1,sum)
+apply(x,2,sum)
+rowSums(x)
+colSums(x)
+rowMeans(x)
+colMeans(x)
+# apply function
+x <- matrix(rnorm(200),20,10)
+apply(x,1,quantile,probs = c(0.25,0.75))
+# tapply function
+
+x <- c(rnorm(10),runif(10),rnorm(10,1))
+f <- gl(3,10)
+f
+tapply(x,f,mean)
+tapply(x,f,mean,simplify = F)
+tapply(x,f,range)
